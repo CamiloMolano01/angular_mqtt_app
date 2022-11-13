f8a8e61c82abd2b7591ce9bddc11104b8d847ab5
Camilo Molano <ecamilo.molano@gmail.com>
2022-11-13 05:20:20 -0500

    add buttons with specific actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ViewChild, ElementRef, OnDestroy  } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'angular_mqtt_app';
@@ -15,7 +21,7 @@ export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('msglog', { static: true })
   msglog!: ElementRef;
 
-  constructor(private _mqttService: MqttService) { }
+  constructor(private _mqttService: MqttService) {}
 
   ngOnInit(): void {}
 
@@ -23,23 +29,50 @@ export class AppComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-
   subscribeNewTopic(): void {
-    console.log('inside subscribe new topic')
-    this.subscription = this._mqttService.observe(this.topicname).subscribe((message: IMqttMessage) => {
-      this.msg = message;
-      console.log('msg: ', message)
-      this.logMsg('Message: ' + message.payload.toString() + '<br> for topic: ' + message.topic);
-    });
-    this.logMsg('subscribed to topic: ' + this.topicname)
+    console.log('inside subscribe new topic');
+    this.subscription = this._mqttService
+      .observe(this.topicname)
+      .subscribe((message: IMqttMessage) => {
+        this.msg = message;
+        console.log('msg: ', message);
+        this.logMsg(
+          'Message: ' +
+            message.payload.toString() +
+            '<br> for topic: ' +
+            message.topic
+        );
+      });
+    this.logMsg('subscribed to topic: ' + this.topicname);
   }
 
   sendmsg(): void {
     // use unsafe publish for non-ssl websockets
-    this._mqttService.unsafePublish(this.topicname, this.msg, { qos: 1, retain: true })
-    this.msg = ''
+    this._mqttService.unsafePublish(this.topicname, this.msg, {
+      qos: 1,
+      retain: true,
+    });
+    this.msg = '';
   }
-  
+
+  send1Action(): void {
+    // use unsafe publish for non-ssl websockets
+    this._mqttService.unsafePublish('expulse', '3', {
+      qos: 1,
+      retain: true,
+    });
+    this.msg = '';
+  }
+
+  send2Action(): void {
+    // use unsafe publish for non-ssl websockets
+    this._mqttService.unsafePublish('move', '5', {
+      qos: 1,
+      retain: true,
+    });
+    this.msg = '';
+  }
+
   logMsg(message: string): void {
     this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Component, NgModule, OnDestroy } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,49 +10,46 @@ import {
   IMqttMessage,
   MqttModule,
   IMqttServiceOptions,
-  MqttService
+  MqttService,
 } from 'ngx-mqtt';
-
 
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   hostname: 'localhost',
   port: 9001,
   //path: '/mqtt',
-  path: ''
+  path: '',
 };
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
 @Component({
-  template: `
-    <h1>ALGO</h1>
-  `
+  template: ` <h1>ALGO</h1> `,
 })
 export class ExampleComponent implements OnDestroy {
   private subscription: Subscription;
   public message: string | undefined;
 
   constructor(private _mqttService: MqttService) {
-    this.subscription = this._mqttService.observe('my/topic').subscribe((message: IMqttMessage) => {
-      this.message = message.payload.toString();
-    });
+    this.subscription = this._mqttService
+      .observe('my/topic')
+      .subscribe((message: IMqttMessage) => {
+        this.message = message.payload.toString();
+      });
   }
 
   public unsafePublish(topic: string, message: string): void {
-    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+    this._mqttService.unsafePublish(topic, message, { qos: 1, retain: true });
   }
 
   public ngOnDestroy() {
