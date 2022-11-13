bb912d82c525ac3ddb2a40d3313d284bd7f72077
Camilo Molano <ecamilo.molano@gmail.com>
2022-11-12 23:02:54 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule, OnDestroy } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { Observable, Subscription } from 'rxjs';
+
+import {
+  IMqttMessage,
+  MqttModule,
+  IMqttServiceOptions,
+  MqttService
+} from 'ngx-mqtt';
+
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 9001,
+  path: '/mqtt'
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +26,34 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+@Component({
+  template: `
+    <h1>ALGO</h1>
+  `
+})
+export class ExampleComponent implements OnDestroy {
+  private subscription: Subscription;
+  public message: string | undefined;
+
+  constructor(private _mqttService: MqttService) {
+    this.subscription = this._mqttService.observe('my/topic').subscribe((message: IMqttMessage) => {
+      this.message = message.payload.toString();
+    });
+  }
+
+  public unsafePublish(topic: string, message: string): void {
+    this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
