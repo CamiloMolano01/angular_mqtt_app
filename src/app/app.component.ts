e11860e4ecaa06e0eab5ed15a4af1ae8efbea298
JohnGranados <jgranados@smartsoft.la>
2022-11-15 21:46:51 -0500

    add finaly changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Subscription } from 'rxjs';
 import { IMqttMessage, MqttService } from 'ngx-mqtt';
 import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
-import {default as Annotation} from 'chartjs-plugin-annotation';
+import { default as Annotation } from 'chartjs-plugin-annotation';
 
 @Component({
   selector: 'app-root',
@@ -24,33 +24,36 @@ export class AppComponent implements OnInit, OnDestroy {
   isConnected: boolean = false;
   @ViewChild('msglog', { static: true })
   msglog!: ElementRef;
-
+  nivelAgua: number[] = [10, 18];
+  number: number = 0;
+  tabla: boolean = false;
 
   constructor(private _mqttService: MqttService) {
     Chart.register(Annotation)
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscribeNewTopic();
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
   subscribeNewTopic(): void {
-    console.log('inside subscribe new topic');
+    var enc = new TextDecoder("utf-8");
     this.subscription = this._mqttService
-      .observe(this.topicname)
-      .subscribe((message: IMqttMessage) => {
-        this.msg = message;
-        console.log('msg: ', message);
-        this.logMsg(
-          'Message: ' +
-            message.payload.toString() +
-            '<br> for topic: ' +
-            message.topic
-        );
+      .observe('chiku/team/levelWater')
+      .subscribe((message: any) => {
+        var arr = new Uint8Array(message['payload']);
+        this.number = Number(enc.decode(arr));
+        this.nivelAgua.push(this.number);
+        this.tabla = true;
+        this.chart?.update();
+        // if (this.nivelAgua.length == 30) {
+        //   this.chart?.update();
+        // }
       });
-    this.logMsg('subscribed to topic: ' + this.topicname);
   }
 
   sendmsg(): void {
@@ -69,7 +72,6 @@ export class AppComponent implements OnInit, OnDestroy {
       retain: true,
     });
     this.msg = '';
-    //this.logMsg('cualquiera');
   }
 
   send2Action(): void {
@@ -90,30 +92,12 @@ export class AppComponent implements OnInit, OnDestroy {
     this.msg = '';
   }
 
-  logMsg(message: string): void {
-    this.msglog.nativeElement.innerHTML += '<br><hr>' + message;
-  }
-
-  clear(): void {
-    this.msglog.nativeElement.innerHTML = '';
-  }
 
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-      {
-        data: [ 28, 48, 40, 19, 86, 27, 90 ],
-        label: 'Series B',
+        data: this.nivelAgua,
+        label: 'Nivel de agua en Tanque',
         backgroundColor: 'rgba(77,83,96,0.2)',
         borderColor: 'rgba(77,83,96,1)',
         pointBackgroundColor: 'rgba(77,83,96,1)',
@@ -121,21 +105,9 @@ export class AppComponent implements OnInit, OnDestroy {
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(77,83,96,1)',
         fill: 'origin',
-      },
-      {
-        data: [ 180, 480, 770, 90, 1000, 270, 400 ],
-        label: 'Series C',
-        yAxisID: 'y-axis-1',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        borderColor: 'red',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
       }
     ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
@@ -148,9 +120,9 @@ export class AppComponent implements OnInit, OnDestroy {
       // We use this empty structure as a placeholder for dynamic theming.
       x: {},
       'y-axis-0':
-        {
-          position: 'left',
-        },
+      {
+        position: 'left',
+      },
       'y-axis-1': {
         position: 'right',
         grid: {
@@ -161,48 +133,12 @@ export class AppComponent implements OnInit, OnDestroy {
         }
       }
     },
-
-    plugins: {
-      legend: { display: true },
-      annotation: {
-        annotations: [
-          {
-            type: 'line',
-            scaleID: 'x',
-            value: 'March',
-            borderColor: 'orange',
-            borderWidth: 2,
-            label: {
-              display: true,
-              position: 'center',
-              color: 'orange',
-              content: 'LineAnno',
-              font: {
-                weight: 'bold'
-              }
-            }
-          },
-        ],
-      }
-    }
   };
 
   public lineChartType: ChartType = 'line';
 
   @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
-  generateNumber(i: number): number {
-    return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
-  }
-
-  public randomize(): void {
-    for (let i = 0; i < this.lineChartData.datasets.length; i++) {
-      for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
-        this.lineChartData.datasets[i].data[j] = this.generateNumber(i);
-      }
-    }
-    this.chart?.update();
-  }
 
   // events
   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
@@ -213,36 +149,4 @@ export class AppComponent implements OnInit, OnDestroy {
     console.log(event, active);
   }
 
-  public hideOne(): void {
-    const isHidden = this.chart?.isDatasetHidden(1);
-    this.chart?.hideDataset(1, !isHidden);
-  }
-
-  public pushOne(): void {
-    this.lineChartData.datasets.forEach((x, i) => {
-      const num = this.generateNumber(i);
-      x.data.push(num);
-    });
-    this.lineChartData?.labels?.push(`Label ${ this.lineChartData.labels.length }`);
-
-    this.chart?.update();
-  }
-
-  public changeColor(): void {
-    this.lineChartData.datasets[2].borderColor = 'green';
-    this.lineChartData.datasets[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-
-    this.chart?.update();
-  }
-
-  public changeLabel(): void {
-    if (this.lineChartData.labels) {
-      this.lineChartData.labels[2] = [ '1st Line', '2nd Line' ];
-    }
-
-    this.chart?.update();
-  }
-  desordenar(){
-
-  }
 }
